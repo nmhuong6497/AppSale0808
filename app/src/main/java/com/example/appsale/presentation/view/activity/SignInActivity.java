40c05436aa775e94792aeb9555e7ccc049656adb
NMH <nm.huong6497@gmail.com>
2022-11-17 05:59:06 +0700

    UI sign in

---
+++ app/src/main/java/com/example/appsale/presentation/view/activity/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.appsale.presentation.view.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appsale.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
