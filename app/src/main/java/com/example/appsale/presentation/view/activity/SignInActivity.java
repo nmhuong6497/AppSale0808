56670d7ca449429b5302fb20deae9a724ef20155
NMH <nm.huong6497@gmail.com>
2022-11-26 08:32:02 +0700

    sign in

--- app/src/main/java/com/example/appsale/presentation/view/activity/SignInActivity.java
+++ app/src/main/java/com/example/appsale/presentation/view/activity/SignInActivity.java
@@ -1,16 +1,58 @@
 package com.example.appsale.presentation.view.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.appsale.R;
+import com.example.appsale.data.model.AppResource;
+import com.example.appsale.data.model.User;
+import com.example.appsale.presentation.viewmodel.SignInViewModel;
 
 public class SignInActivity extends AppCompatActivity {
+
+    SignInViewModel signInViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        signInViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new SignInViewModel(SignInActivity.this);
+            }
+        }).get(SignInViewModel.class);
+
+        observer();
+        event();
+    }
+
+    private void observer() {
+        signInViewModel.getUserResource().observe(this, new Observer<AppResource<User>>() {
+            @Override
+            public void onChanged(AppResource<User> userAppResource) {
+                switch (userAppResource.status) {
+                    case ERROR:
+                        Toast.makeText(SignInActivity.this, userAppResource.message, Toast.LENGTH_SHORT).show();
+                        break;
+                    case LOADING:
+                        break;
+                    case SUCCESS:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void event() {
+        signInViewModel.signIn("nm.huong6497@gmail.com", "12345678");
     }
 }
